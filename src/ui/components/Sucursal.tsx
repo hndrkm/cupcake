6d16f894b51fbbc31985ab5888dd764d371f83ab
flynth_dev <flynth.dev@gmail.com>
2024-07-26 19:04:35 -0400

    About and Contact was done

---
+++ src/ui/components/Sucursal.tsx
@@ -0,0 +1,54 @@
+import Image from "next/image"
+
+export function Sucursal (
+  props:{titulo_sucursal:string,img:string,src_map:string,titulo_descripcion:string,descripcion:string})
+{
+  return(
+
+    <div className="flex flex-col py-10">
+      <div className="shadow-lg hover:shadow-lg hover:shadow-primary group duration-700 hover:scale-105  ">
+
+        <h2 className="text-2xl font-bold tracking-tight text-text-primary py-10">{props.titulo_sucursal}</h2>
+
+        <div className="w-full  mb-4 bg-background  ">
+          <div className="flex items-center bg-white ">
+
+            <div className="relative flex flex-row justify-center  items-center w-3/6 h-96 m-5">
+              <Image
+                className=""
+                src={props.img}
+                fill={true}
+                quality={100}
+                objectFit="cover"
+                alt="Picture of the author"
+              />
+            </div>
+
+            <div className="relative flex flex-row justify-center  items-center w-3/6 h-96 m-5">
+              <div className="w-full h-full max-w-4xl mx-auto my-8">
+                <iframe
+                  src={props.src_map}
+                  width="100%"
+                  style={{ border: '0' }}
+                  allowFullScreen
+                  loading="lazy"
+                  referrerPolicy="no-referrer-when-downgrade"
+                  className="rounded-lg shadow-lg h-96 border-none  "
+                />
+              </div>
+            </div>
+
+          </div>
+        </div>
+
+        <div className="p-10 flex items-center justify-center ">
+          <div className="text-left">
+            <h2 className="text-3xl mb-4 font-semibold text-text-primary py-8">{props.titulo_descripcion}</h2>
+            <p className="text-gray-700 pb-8">{props.descripcion}</p>
+          </div>
+        </div>
+
+      </div>
+    </div>
+      )
+}
